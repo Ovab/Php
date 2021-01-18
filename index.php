1949df5e202dc4486c1c6c38599bd479d4d0d0aa
Ovab <bavo.famknol@gmail.com>
2021-01-18 14:12:53 +0100

    added links to index

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
 <a href="index.php">Home</a>
 <a href="Opdr%202.php">Opdracht 2</a>
 <a href="Opdracht3/3.1.php">Opdracht 3.1.1</a>
+<a href="Opdracht4/4.php">Opdracht 4</a>
+<a href="Opdracht5/Form.html">Opdracht 5</a>
 <?php echo '<p>Hello World</p>'; ?>
 </body>
 </html>
