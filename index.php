c113d071860d9319688127ece2993370385ddadd
Bavo <61050421+Ovab@users.noreply.github.com>
2021-01-25 08:43:44 +0100

    Added some links to the home page

--- index.php
+++ index.php
@@ -4,10 +4,12 @@
 </head>
 <body>
 <a href="index.php">Home</a>
-<a href="Opdr%202.php">Opdracht 2</a>
+<a href="Opdracht2/Opdr%202.php">Opdracht 2</a>
 <a href="Opdracht3/3.1.php">Opdracht 3.1.1</a>
 <a href="Opdracht4/4.php">Opdracht 4</a>
 <a href="Opdracht5/Form.html">Opdracht 5</a>
+<a href="Opdracht6/login.html">Opdracht 6</a>
+<a href="Opdracht7/LoginPage.html">Opdracht 7</a>
 <?php echo '<p>Hello World</p>'; ?>
 </body>
 </html>
